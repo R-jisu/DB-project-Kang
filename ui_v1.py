1567c674a4d4054d5975a1cbd4cabca101fbb34b
R-jisu <79021544+R-jisu@users.noreply.github.com>
2022-12-14 20:26:18 +0900

    window

--- ui_v1.py
+++ ui_v1.py
@@ -7,7 +7,6 @@ from PyQt5 import QtGui
 import api_get
 import requests
 
-
 # UI파일 연결
 # 단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
 form_recipeclass = uic.loadUiType("recipe.ui")[0]
@@ -21,7 +20,7 @@ class WindowClass(QMainWindow, form_mainclass):
         super().__init__()
         self.setupUi(self)
 
-        self.search.clicked.connect(self.openCaptureClass)
+        self.search.clicked.connect(self.goSearchWindow)
         self.ADD.clicked.connect(self.ADDFunction)
         self.DELETE.clicked.connect(self.DELFunction)
 
@@ -31,16 +30,18 @@ class WindowClass(QMainWindow, form_mainclass):
     def openCaptureClass(self):
         # search = searchwindow(self.searchtext.toPlainText())
         # search.exec()
+        foodname=searchwindow.findChildren(searchwindow,QTextEdit,'S_searchtext')
+        print(foodname)
         widget.setCurrentIndex(widget.currentIndex()+1)
-        self.searchtext.setText('')
+        # self.searchtext.setText('')
 
     def goSearchWindow(self):
-        self.hide()  # 메인윈도우 숨김
+        WindowClass().close()  # 메인윈도우 숨김
         self.search = searchwindow(self.searchtext.toPlainText())
         self.search.exec()  # search 창 닫을 때까지 기다림
         self.searchtext.setText('')
         print(1)
-        self.show()  # search 창을 닫으면 다시 첫 번째 창이 보여짐
+        WindowClass().show()  # search 창을 닫으면 다시 첫 번째 창이 보여짐
 
     def ADDFunction(self):
         # ADD 버튼 눌릴 시
@@ -73,13 +74,13 @@ class searchwindow(QDialog, QWidget, form_searchclass):
 
         self.searching()
 
-        self.S_BackBtn.clicked.connect(self.openCaptureClass)
+        self.S_BackBtn.clicked.connect(self.goMainWindow)
         self.S_searchBtn.clicked.connect(self.searching)
         self.S_searchBtn.clicked.connect(self.searching)
-        self.S_tableWidget.doubleClicked.connect(self.openCaptureClass1)
+        self.S_tableWidget.doubleClicked.connect(self.goRecipeWindow)
 
     def openCaptureClass(self):
-         widget.setCurrentIndex(widget.currentIndex()-1)
+        widget.setCurrentIndex(widget.currentIndex()-1)
 
     def openCaptureClass1(self):
         widget.setCurrentIndex(widget.currentIndex()+1)
@@ -88,11 +89,11 @@ class searchwindow(QDialog, QWidget, form_searchclass):
         self.setupUi(self)
 
     def goRecipeWindow(self):
-        self.hide()  # 윈도우 숨김
-        recipe = recipewindow()
-        recipe.exec()  # recipe 창 닫을 때까지 기다림
+        searchwindow(self.foodname).close()  # 윈도우 숨김
+        self.recipe = recipewindow()
+        self.recipe.exec()  # recipe 창 닫을 때까지 기다림
         print(2)
-        self.show()  # recipe 창을 닫으면 다시 첫 번째 창이 보여짐
+        searchwindow(self.foodname).show()  # recipe 창을 닫으면 다시 첫 번째 창이 보여짐
 
     def goMainWindow(self):
         self.close()  # 클릭시 종료됨.
@@ -100,15 +101,15 @@ class searchwindow(QDialog, QWidget, form_searchclass):
     def searching(self):
         self.Tuples = 0 # 표 start
         self.S_searchtext.setText(self.foodname)
-        if self.S_searchtext.toPlainText() == '':
-            print("noThing")
-        else:
+        if self.S_searchtext.toPlainText() != '':
             res = requests.get(api_get.getURL(
                 self.url, self.key, self.S_searchtext.toPlainText()))
             recipe = res.json()
             for a in recipe['COOKRCP01']['row']:
                 self.S_tableWidget.setItem(self.Tuples,0,QTableWidgetItem(a['RCP_NM'])) # 행 열 데이터
                 self.Tuples+=1 # 위에부터 표 채우기
+        else:
+            print("noThing")
 
     def table_doubleClicked(self):
         row = self.S_tableWidget.currentIndex().row()
@@ -122,7 +123,7 @@ class recipewindow(QDialog, QWidget, form_recipeclass):
         self.initUi()
         self.show()
 
-        self.R_BackBtn.clicked.connect(self.openCaptureClass)
+        self.R_BackBtn.clicked.connect(self.goSearchWindow)
 
     def openCaptureClass(self):
          widget.setCurrentIndex(widget.currentIndex()-1)
@@ -139,18 +140,18 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     mainWindow = WindowClass()
-    searchWindow = searchwindow(foodname='')
-    recipeWindow = recipewindow()
+    # searchWindow = searchwindow(foodname='')
+    # recipeWindow = recipewindow()
 
-    widget = QStackedWidget()
-    widget.addWidget(mainWindow)
-    widget.addWidget(searchWindow)
-    widget.addWidget(recipeWindow)
+    # widget = QStackedWidget()
+    # widget.addWidget(mainWindow)
+    # widget.addWidget(searchWindow)
+    # widget.addWidget(recipeWindow)
 
-    widget.setFixedHeight(479)
-    widget.setFixedWidth(380)
-    widget.show()
+    # widget.setFixedHeight(479)
+    # widget.setFixedWidth(380)
+    # widget.show()
     
-    # mainWindow.show()
+    mainWindow.show()
 
     app.exec_()
